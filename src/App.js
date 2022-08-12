8be9d5a981bd8428b897e28213467dc9a316d2cf
özcan çetin <cetin445@gmail.com>
2022-08-12 21:27:00 +0300

    v5

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
       <h1 className="text-center text-capitalize">reviews</h1>
       <div className="box d-flex justify-content-center text-center mt-4">
         <span className="left">
-          <FiChevronLeft onClick={()=>setIndex(index - 1)}/>
+          <FiChevronLeft onClick={()=>setIndex(index > 0 ? index - 1 : people.lengt - 1)}/>
         </span>
         <Slider people={people} index={index} setIndex={setIndex} />
         <span className="right">
-          <FiChevronRight onClick={()=>setIndex(index + 1)}/>
+          <FiChevronRight onClick={()=>setIndex(index < people.length -1 ? index + 1 : 0)}/>
         </span>
       </div>
     </div>
